b41a23a31dc23a64ea7ed8c8f24ce79dac2989f0
anumulaa7619 <50210625+anumulaa7619@users.noreply.github.com>
2019-06-04 18:15:30 -0700

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -7,6 +7,7 @@ float areaOfCircle(float r)
   float area = (M_PI * r * r);
   return area;
 }
+
 //This is an function defined before the int main which returns an area us$
 
 int main(int argc, char* argv[])
@@ -18,30 +19,64 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  // at this point we know we have two args, let's check that they are ints
-  int arg1;
-  // ssscanf scans a string for a format - in this case an integer (%d) and returns
+  // at this point we know we have two args, let's check that they are floats
+  float arg1;
+  // ssscanf scans a string for a format - in this case an float (%f) and returns
   // the number of items found
-  int found = sscanf(argv[1], "%d", &arg1);
+  int found = sscanf(argv[1], "%f", &arg1);
   if (found != 1)
   {
-    printf("first arg is not an integer, enter two ints\n");
+    printf("first arg is not a valid number\n");
     return 1;
   }
 
-  int arg2;
-  found = sscanf(argv[2], "%d", &arg2);
+  float arg2;
+  found = sscanf(argv[2], "%f", &arg2);
   if (found != 1)
   {
-    printf("second arg is not an integer, enter two ints\n");
+    printf("second arg is not a valid input\n");
     return 1;
   }
-  float small, big;     //Here we initialize 2 variables where the code wi$
-                      //take user input
   
-  for (float r = arg1; r <= arg2; r++)          //Interates through the in$
+  char arg3;
+  found = sscanf(argv[3], "%c", &arg3);
+  if (found != 1)
   {
-        float compute = areaOfCircle(r);
-        printf("A circle of r = %f is has area = %f\n", r, compute);
+    printf("operation is not defined, enter a valid operation\n");
+    return 1;
   }
+  
+  //for (float r = arg1; r <= arg2; r++)          //Interates through the in$
+  //{
+  //      float compute = areaOfCircle(r);
+  //      printf("A circle of r = %f is has area = %f\n", r, compute);
+  //}
+  
+  if (char arg3 == "+")
+  {
+    float sum = arg1 + arg2;
+    printf("%f\n", sum);
+  }
+  
+  if (char arg3 == "-")
+  {
+    float diff = arg1 - arg2;
+    printf("%f\n", diff);
+  }
+  
+  if (char arg3 == "*")
+  {
+    float prod = arg1 * arg2;
+    printf("%f\n", prod);
+  }
+  
+  if (char arg3 == "-")
+  {
+    float quot = arg1 / arg2;
+    printf("%f\n", quot);
+  }
+  
+  
+  
+  
 }
