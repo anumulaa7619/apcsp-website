e245be5baab1dfc622b8815b4995dcccd84c546a
anumulaa7619 <50210625+anumulaa7619@users.noreply.github.com>
2019-06-07 08:04:16 -0700

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -48,7 +48,7 @@ int main(int argc, char* argv[])
   char minus[10] = "-";
   char mul[10] = "*";
   char div[10] = "/";
-  char fact[10] = "!";
+  char fact[10] = "fact";
   char mod[10] = "%";
   float ans;
   //for (float r = arg1; r <= arg2; r++)          //Interates through the in$
