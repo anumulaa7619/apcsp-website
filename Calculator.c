c43a5188b68543ea381a44ff535729227795a50f
eightblades <32313740+eightblades@users.noreply.github.com>
2019-06-10 08:33:12 -0700

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -33,21 +33,21 @@ int main(int argc, char* argv[]){
   
   
   
-  char arg4[10];
-  int found = sscanf(argv[1], "%s", arg4);
+  char arg1[10];
+  int found = sscanf(argv[1], "%s", arg1);
   
   
-  float arg1;
-  found = sscanf(argv[2], "%f", &arg1);
+  float arg2;
+  found = sscanf(argv[2], "%f", &arg2);
   if (found != 1){
     printf("first arg is not a valid number\n");
     return 1;
   }
 
   
-  float arg3;
+  float arg4;
   if (argc > 4){
-  found = sscanf(argv[3], "%f", &arg3);
+  found = sscanf(argv[4], "%f", &arg4);
   }
   //if (found != 1)
   //{
@@ -69,39 +69,39 @@ int main(int argc, char* argv[]){
 
 
 
-  if (strcmp(arg4, "s") == 0){
-    char arg2[50];
-    found = sscanf(argv[3], "%s", arg2);
-    if (strcmp(arg2, add) == 0){
-      ans = arg1 + arg3;
+  if (strcmp(arg1, "s") == 0){
+    char arg3[50];
+    found = sscanf(argv[3], "%s", arg3);
+    if (strcmp(arg3, add) == 0){
+      ans = arg2 + arg3;
       printf("%f\n", ans);
-    } else if (strcmp(arg2, minus) == 0){
-      ans = arg1 - arg3;
+    } else if (strcmp(arg3, minus) == 0){
+      ans = arg2 - arg3;
       printf("%f\n", ans);
-    } else if (strcmp(arg2, mul) == 0){
-      ans = arg1 * arg3;
+    } else if (strcmp(arg3, mul) == 0){
+      ans = arg2 * arg3;
       printf("%f\n", ans);
-    } else if (strcmp(arg2, div) == 0){
-      ans = arg1 / arg3;
+    } else if (strcmp(arg3, div) == 0){
+      ans = arg2 / arg3;
       printf("%f\n", ans);
-    } else if (strcmp(arg2, fact) == 0){
+    } else if (strcmp(arg3, fact) == 0){
       ans = 1;
-      for (int i = arg1; i > 1; i = i -1){
+      for (int i = arg2; i > 1; i = i -1){
         ans = ans * i;
       }
       printf("%f\n", ans);
-    } else if (strcmp(arg2, "mod") == 0){
-      mo(arg1, arg3);
+    } else if (strcmp(arg3, "mod") == 0){
+      mo(arg2, arg3);
     } else{
       printf("Invalid Operation");
     }
-  } else if (strcmp(arg4, "aoc") == 0){
-    ans = aoc(arg1);
+  } else if (strcmp(arg1, "aoc") == 0){
+    ans = aoc(arg2);
     printf("%f", ans);
-  } else if (strcmp(arg4, "aot") == 0){
-    float arg2;
-    sscanf(argv[3], "%f", &arg2);
-    ans = aot(arg1, arg2);
+  } else if (strcmp(arg1, "aot") == 0){
+    float arg3;
+    sscanf(argv[3], "%f", &arg3);
+    ans = aot(arg2, arg3);
     printf("%f", ans);
   } else {
     printf("Invalid Calc Type");
@@ -114,26 +114,26 @@ int main(int argc, char* argv[]){
 
 
 
-  //if (strcmp(arg2, "area of circle") == 0){
+  //if (strcmp(arg3, "area of circle") == 0){
     //ans = aoc(arg1);
     //printf("%f", ans);
-  //} else if (strcmp(arg2, "area of triangle") == 0){
+  //} else if (strcmp(arg3, "area of triangle") == 0){
     //ans = aot(arg1, arg3);
    // printf("%f", ans);
   //} 
-  //else if (strcmp(arg2, add) == 0){
+  //else if (strcmp(arg3, add) == 0){
   //  ans = arg1 + arg3;
   //  printf("%f\n", ans);
-  //} else if (strcmp(arg2, minus) == 0){
+  //} else if (strcmp(arg3, minus) == 0){
   //    ans = arg1 - arg3;
   //    printf("%f\n", ans);
-  //} else if (strcmp(arg2, mul) == 0){
+  //} else if (strcmp(arg3, mul) == 0){
   //  ans = arg1 * arg3;
   //   printf("%f\n", ans);
-  //} else if (strcmp(arg2, div) == 0){
+  //} else if (strcmp(arg3, div) == 0){
   //    ans = arg1 / arg3;
   //    printf("%f\n", ans);
-  //} else if (strcmp(arg2, fact) == 0){
+  //} else if (strcmp(arg3, fact) == 0){
   //   printf("test");
   //    found = sscanf(argv[1], "%d", &arg1);
   //} else{
